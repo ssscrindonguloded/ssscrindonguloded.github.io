46b7d184de775dcd5938b80bbf2dd93ae1880776
sduluc <>
2025-01-09 22:19:39 +0100

    ajours

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
+let xScale = 0.015;
+let yScale = 0.02;
 
+function setup() {
+  let canvas = createCanvas(windowWidth-17, 100);
+  canvas.parent('crea');  
+  gap = 15;
+  offset = 105;
+  dotGrid();
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-    var images = document.querySelectorAll('.retex1, .retex2');
-    
-    images.forEach(function (image) {
-        
-        
-        image.addEventListener('click', function () {
-            image.classList.toggle('zoomed');
-        });
-    });
-});
+function dotGrid() {
+  background(65, 65, 65);
+  noStroke();
+  fill(255);
 
-
-
-
+  for (let x = gap / 2; x < width; x += gap) {
+    for (let y = gap / 2; y < height; y += gap) {
+      let noiseValue = noise((x + offset) * xScale, (y + offset) * yScale);
+      let diameter = noiseValue * gap;
+      circle(x, y, diameter);
+    }
+  }
+}
